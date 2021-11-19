ba02f9e600eb64961ddb6a28891ac3b792e13f11
NicolaL95 <nicola.lilla@gmail.com>
2021-11-19 16:56:13 +0100

    Fix merge array

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -4,13 +4,27 @@
       <input v-model="customQuery" type="text" />
       <button v-on:click="getURL()" type="button">Click me!</button>
     </div>
-    <div class="container">
-      <div class="movie" v-for="movie in movies" :key="movie.id">
-        <p class="title">{{ movie.title }}</p>
-        <p class="original_title">{{ movie.original_title }}</p>
-        <Flag :selCountry="movie.original_language" />
-        <p class="original_language">{{ movie.original_language }}</p>
-        <p class="vote_average">{{ movie.vote_average }}</p>
+
+    <div
+      class="multimedia"
+      v-for="multimedia in multimedias"
+      :key="multimedia.id"
+    >
+      <div v-if="multimedia.genre == 'film'" class="movie_container">
+        <h1>FILM</h1>
+        <p class="title">{{ multimedia.title }}</p>
+        <p class="original_title">{{ multimedia.original_title }}</p>
+        <Flag :selCountry="multimedia.original_language" />
+        <p class="original_language">{{ multimedia.original_language }}</p>
+        <p class="vote_average">{{ multimedia.vote_average }}</p>
+      </div>
+      <div v-else class="tv_container">
+        <h1>SERIE TV</h1>
+        <p class="name">{{ multimedia.title }}</p>
+        <p class="original_name">{{ multimedia.original_title }}</p>
+        <Flag :selCountry="multimedia.original_language" />
+        <p class="original_language">{{ multimedia.original_language }}</p>
+        <p class="vote_average">{{ multimedia.vote_average }}</p>
       </div>
     </div>
   </div>
@@ -26,23 +40,29 @@ export default {
   data() {
     return {
       customQuery: "",
-      movies: [],
+      multimedias: [],
     };
   },
   methods: {
+    /* get two Apis and merge them in an array  */
     getURL() {
       const API_URLF =
         "https://api.themoviedb.org/3/search/movie?api_key=fd4723f70e60dc27b6383adc8e7700ec&query=" +
-        this.customQuery +
-        "&language=en-US&page=1&include_adult=false";
+        this.customQuery;
       const API_URLT =
         "https://api.themoviedb.org/3/search/tv?api_key=fd4723f70e60dc27b6383adc8e7700ec&query=" +
-        this.customQuery +
-        "&language=en-US&page=1&include_adult=false";
+        this.customQuery;
+      console.log(API_URLF, API_URLT);
       axios.all([axios.get(API_URLF), axios.get(API_URLT)]).then((r) => {
-        console.log(r[0].data.results, r[1].data.results);
-        this.movies = r[0].data.results && r[1].data.results;
-        console.log(this.movies);
+        /* set properties to specify type of content and add to array multimedias */
+        r[0].data.results.forEach((element) => {
+          element.genre = "film";
+          this.multimedias.push(element);
+        });
+        r[1].data.results.forEach((element) => {
+          element.genre = "tv";
+          this.multimedias.push(element);
+        });
       });
     },
   },
